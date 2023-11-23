49360ea72eb1757dd5a1b90800db980a429ae491
Sanyam Rawat <126306140+Yamyamlol@users.noreply.github.com>
2023-11-23 19:05:27 +0530

    Update faces.py

--- faces.py
+++ faces.py
@@ -2,58 +2,104 @@ import tkinter as tk
 from tkinter import filedialog
 import face_recognition
 import shutil
+import cv2
 import os
 import csv
+import numpy as np
 
 class ArrayManager:
     encodings = []
     names = []
 
-def copy_to_directory(file_path, name):
-    destination_directory = os.path.abspath("D:/Coding/mini project/images")  # Update with your actual directory
+def copy_to_directory(image, name, destination_directory):
     if not os.path.exists(destination_directory):
         os.makedirs(destination_directory)
-    destination_path = os.path.join(destination_directory, f"{name}.jpg")
-    shutil.copy(file_path, destination_path)
+
+    for i in range(1, 11):
+        destination_path = os.path.join(destination_directory, f"{name}{i}.jpg")
+        cv2.imwrite(destination_path, image)
+        print(f'The captured image has been saved to {destination_path}')
     return destination_path
 
 def save_to_csv(names, encodings):
-    csv_file_path = os.path.join('D:\Coding\mini project\database', "Data.txt")
-    with open(csv_file_path, mode='w', newline='') as file:
+    csv_file_path = os.path.join('D:/Coding/mini project/database', "Data.txt")
+
+    # Check if the file exists
+    file_exists = os.path.exists(csv_file_path)
+
+    with open(csv_file_path, mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(["Name", "Encoding"])
+
+        # Write the header if the file is newly created or empty
+        if not file_exists or os.stat(csv_file_path).st_size == 0:
+            writer.writerow(["Name", "Encoding"])
+
+        # Write the data for each encoding
         for name, encoding in zip(names, encodings):
-            writer.writerow([name, encoding])
+            # Normalize the encoding
+            encoding = encoding / np.linalg.norm(encoding)
+
+            # Convert encoding values to strings
+            encoding_strings = [str(value) for value in encoding]
+            writer.writerow([name] + encoding_strings)
+
     return csv_file_path
 
-def upload_file(array_manager, name_entry, directory):
-    file_path = filedialog.askopenfilename(title="Select a file")
-    if file_path:
-        print("Selected file:", file_path)
+def capture_images(array_manager, name_entry):
+    if len(array_manager.names) > 0:
+        print("You have already entered a name.")
+        return
 
-        # Get the name from the entry widget
-        name = name_entry.get()
-        if not name:
-            print("Please enter a name.")
-            return
+    # Get the name from the entry widget
+    name = name_entry.get()
+    if not name:
+        print("Please enter a name.")
+        return
+
+    # Open webcam
+    cap = cv2.VideoCapture(0)
+
+    # Create a window to display the video feed
+    cv2.namedWindow("Capture Face", cv2.WINDOW_NORMAL)
+
+    # Counter for the number of frames with detected faces
+    face_count = 0
+
+    while face_count < 10:
+        ret, frame = cap.read()
 
         # Encode the face
-        face_image = face_recognition.load_image_file(file_path)
-        encoding = face_recognition.face_encodings(face_image)[0]
+        face_locations = face_recognition.face_locations(frame)
+        if face_locations:
+            encoding = face_recognition.face_encodings(frame, face_locations)[0]
 
-        # Store the name and encoding in the arrays
-        array_manager.names.append(name)
-        array_manager.encodings.append(encoding)
-        print("Names:", array_manager.names)
-        print("Encodings:", array_manager.encodings)
+            # Store the name and encoding in the arrays
+            array_manager.names.append(name)
+            array_manager.encodings.append(encoding)
+            print("Names:", array_manager.names)
+            print("Encodings:", array_manager.encodings)
 
-        # Copy the file to the destination directory
-        destination_path = copy_to_directory(file_path, name)
-        print(f'The selected image has been copied to {destination_path}')
+            # Save to destination directory
+            destination_directory = os.path.abspath("D:/Coding/mini project/images")  # Update with your actual directory
+            destination_path = copy_to_directory(frame, name, destination_directory)
 
-        # Save to CSV file
-        csv_file_path = save_to_csv(array_manager.names, array_manager.encodings)
-        print(f'Data has been saved to {csv_file_path}')
+            # Save to CSV file
+            csv_file_path = save_to_csv(array_manager.names, array_manager.encodings)
+            print(f'Data has been saved to {csv_file_path}')
+
+            # Increment the face count
+            face_count += 1
+        else:
+            print("No face found in the captured image.")
+
+        # Display the video feed in the window
+        cv2.imshow("Capture Face", frame)
+        cv2.waitKey(1)  # Adjust the delay (milliseconds) based on your system
+
+    # Release the webcam and destroy the window
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 def main():
     # Create the main window
@@ -72,7 +118,7 @@ def main():
 
     # Function to handle button click
     def on_button_click():
-        upload_file(array_manager, name_entry, destination_directory)
+        capture_images(array_manager, name_entry)
 
     # Create a button widget
     add_face_button = tk.Button(root, text="Add Face", cursor="hand2", width=28, command=on_button_click)
